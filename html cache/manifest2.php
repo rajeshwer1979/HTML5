d0754f29a5bb24178f4a01794c0dfa9ddc24df88
Rajeshwer <31434825+rajeshwer1979@users.noreply.github.com>
2024-12-13 06:30:22 +0530

    Offline loading using HTML Cache
    Offline loading using HTML Cache

---
+++ html cache/manifest2.php
@@ -0,0 +1,21 @@
+<?php
+
+ header('Content-Type: text/cache-manifest');
+ echo "CACHE MANIFEST\n";
+ 
+ 
+echo "CACHE:\n";
+
+ echo "index.html\n";
+ echo "styles/style.css\n";
+  echo "fonts/OpenSans-Regular.ttf\n";
+    echo "assets/logo.png\n";
+  echo "assets/logo1.png\n";
+     echo "assets/settings.png\n";
+   echo "assets/settings1.png\n";
+
+	 
+	 
+	 echo "NETWORK:\n";
+ echo "*";
+?>
